3317848e4f7ad72ce0177a5aded6172d0412d6e8
Lucky13identify <108291368+Lucky13identify@users.noreply.github.com>
2023-06-21 13:28:59 +0300

    s

--- server.js
+++ server.js
@@ -16,7 +16,6 @@ mongoose
     console.log(error.message);
     process.exit(1);
   });
-// sdsdsdsdksdjsdj
 
 // app.listen(3000, () => {
 //   console.log("Server running. Use our API on port: 3000");
