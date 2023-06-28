4b16647e10d1af90e3757d4a9ab6c1e36f046b4e
Lucky13identify <108291368+Lucky13identify@users.noreply.github.com>
2023-06-28 14:39:11 +0300

    new

--- app.js
+++ app.js
@@ -1,23 +1,20 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
+require("dotenv").config();
 
+const authRouter = require("./routes/api/auth");
 const contactsRouter = require("./routes/api/contacts");
 
-// создаем сервер основной
 const app = express();
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
-// ?
-app.use(logger(formatsLogger));
 
-// cors - разрешает кроссдоменный запрос ( запрет серверов КОРС)
+app.use(logger(formatsLogger));
 app.use(cors());
-// для передачи тела в JSON формате (  когда записываем на сервер)
 app.use(express.json());
 
-// (основной запрос, все последующие ищи в этом файле)
-
+app.use("/api/auth", authRouter);
 app.use("/api/contacts", contactsRouter);
 
 app.use((req, res) => {
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const app = require("./app");
 
-const { DB_HOST } = process.env;
+const { DB_HOST, PORT } = process.env;
 
 mongoose.set("strictQuery", true);
 
@@ -9,7 +9,7 @@ mongoose.set("strictQuery", true);
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(3000);
+    app.listen(PORT);
   })
   .catch((error) => {
     console.log(error.message);
