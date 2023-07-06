aad2742fe322f698dfaa355e9a40b125fee8bf11
Lucky13identify <108291368+Lucky13identify@users.noreply.github.com>
2023-07-06 20:16:54 +0300

    new

--- models/user.js
+++ models/user.js
@@ -31,6 +31,14 @@ const userSchema = new Schema(
       type: String,
       required: true,
     },
+    verify: {
+      type: Boolean,
+      default: false,
+    },
+    verificationCode: {
+      type: String,
+      default: "",
+    },
   },
   { versionKey: false, timestamps: true }
 );
@@ -47,10 +55,14 @@ const loginSchema = Joi.object({
   email: Joi.string().required(),
   password: Joi.string().min(6).required(),
 });
+const emailSchema = Joi.object({
+  email: Joi.string().required(),
+});
 
 const schemas = {
   registerSchema,
   loginSchema,
+  emailSchema,
 };
 
 const User = model("user", userSchema);
